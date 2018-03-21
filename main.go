a0db603831fb9e2dbf4e33abc95fbe97d1b4429f
Hinrek Saar <hinrek@gmail.com>
2018-03-21 17:37:24 +0200

    main.go: added qualification standard; user routes & automigrate

--- main.go
+++ main.go
@@ -14,15 +14,20 @@ func setupRouter() *gin.Engine {
 	v1 := router.Group("/api/v1")
 	{
 		v1.POST("/auth/register", models.CreateUser)
-		//v1.GET("/user/:id", models.GetUser)
-		//v1.GET("/users", models.GetAllUsers)
+
+		v1.GET("/user/:id", models.GetUser)
+		v1.GET("/users", models.GetAllUsers)
+
+		v1.GET("/standard/:id", models.GetQualificationStandard)
+		v1.POST("/standard/create", models.CreateQualificationStandard)
+		v1.GET("/standards", models.GetAllQualificationStandards)
 	}
 	return router
 }
 
 func main() {
 	config.InitDB("root:example@tcp(172.18.0.2:3306)/Padevusplatvorm?charset=utf8&parseTime=True&loc=Local")
-	config.DB.AutoMigrate(&models.User{})
+	config.DB.AutoMigrate(&models.User{}, &models.QualificationStandard{}, &models.Competency{}, &models.Knowledge{}, &models.Skill{})
 
 	r := setupRouter()
 	r.Run(":8080")
